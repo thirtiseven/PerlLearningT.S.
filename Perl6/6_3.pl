f7e599ce370ae6fd47a839e2a1c457f9f175b956
thirtiseven lee <ntlihy@gmail.com>
2015-05-07 21:25:38 +0800

    ?

--- Perl6/6_3.pl
+++ Perl6/6_3.pl
@@ -1,11 +1,12 @@
+#这题我不会~~~
 my $alice = 0;
-foreach my $bob(keys %ENV) 
+foreach my $bob(keys %ENV)
 {
-	my $length = length($key);
-	$longest = $length if $length > $longest;
+    my $length = length($key);
+    $longest = $length if $length > $longest;
 }
 
 foreach my $bob(sort keys %ENV)
 {
-	printf "%-${longest}s %s\n",$bob,$ENV{$bob};
-} 
+    printf "%-${longest}s %s\n",$bob,$ENV{$bob};
+}
