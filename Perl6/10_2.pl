79d85cea3edc8a1fc4a68c3c5f0b067d5e51c94c
thirtiseven lee <ntlihy@gmail.com>
2015-05-22 07:58:26 +0800

    perl

---
+++ Perl6/10_2.pl
@@ -0,0 +1,19 @@
+my $Debug = $ENV {DEBUG} // 1;
+my $secret = int(1 + rand 100);
+print "Don't tell anyone,but the secret number is $secret.\n"
+	if $Debug;
+while(1)
+{
+	chomp(my $guess = <STDIN>);
+	if ($guess =~ /quit|exit/) 
+	{
+		last;
+	}elsif ($guess == $secret) {
+		print "Bingo!\n";
+		last;
+	}elsif ($guess > $secret) {
+		print "Too high\n";
+	}else{
+		print "Too low\n";
+	}
+}
